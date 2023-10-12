161bf67c9e73d6e2e16d3faa97a1ae447af02257
Christina Kozanian <christinakozanian@gmail.com>
2023-10-12 14:38:49 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let index = word.length-1
+  for (const char in word) {
+    if (word.charAt(char) === word.charAt(index)){
+      index = index-1
+      return true
+      }
+    else {
+      return false
+    }
+  }
 }
 
 /* 
-  Add your pseudocode here
+initialize constants for first and last indexs
+  check if each first letter matches the last letter, if second letter matches second to last letter.. etc.
+    if palindrome
+      return true
+    if not palindrome
+      return false
 */
 
 /*
-  Add written explanation of your solution here
+-Function initializes the index the last letter
+-It iterates over the string of (word) with a for loop
+-Checks if the first character of that word equals the last letter
+-If true, it decreases in index number by 1 and when the loop continues, the char variable is 
+automatically updated to the next character in the string.
+-If function does not find equal characters, it returns false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,7 +36,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log('=>', isPalindrome('madam'));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
